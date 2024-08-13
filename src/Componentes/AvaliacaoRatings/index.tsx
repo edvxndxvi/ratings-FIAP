b8227b2b84f6d7ccbfefcccbeeeaffd6096dd0ad
edvxndxvi <edvandavi1@hotmail.com>
2024-08-13 10:52:18 -0300

    Alteração de Main, adicionando componente Avaliação Ratings

---
+++ src/Componentes/AvaliacaoRatings/index.tsx
@@ -0,0 +1,14 @@
+import "./index.css"
+import estrela_preenchida from "../../assets/svg/estrela-preenchida.svg"
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
+
+function AvaliacaoRatings(props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }){
+    return(
+        <div className="nota-avaliacao">
+            <img src={estrela_preenchida} alt="Estrela Preenchida Icon" />
+            <p className="branco">{props.name}<span className="cinza">/10</span></p>
+        </div>
+    )
+}
+
+export default AvaliacaoRatings;
