84a3ddd9b295083cf659668727082295482d1f9a
edvxndxvi <edvandavi1@hotmail.com>
2024-08-19 12:41:12 -0300

    Adicionando responsividade ao Main

--- src/Componentes/Main/index.tsx
+++ src/Componentes/Main/index.tsx
@@ -16,18 +16,20 @@ function Main(props:{
             <div className="conteudo-main">
                 <div className="titulo-main">
                     <h1 className="branco">{props.titulo}</h1>
-                    <div className="avaliacao-ratings">
-                        <h2 className="branco">AVALIAÇÃO DA RATINGS</h2>
-                        <div className="nota-avaliacao">
-                            <img src={estrela_preenchida} alt="Estrela Preenchida Icon" />
-                            <p className="branco">{props.nota}<span className="cinza">/10</span></p>
+                    <div className="notas">
+                        <div className="avaliacao-ratings">
+                            <h2 className="branco">AVALIAÇÃO DA RATINGS</h2>
+                            <div className="nota-avaliacao">
+                                <img src={estrela_preenchida} alt="Estrela Preenchida Icon" />
+                                <p className="branco">{props.nota}<span className="cinza">/10</span></p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="avaliacao-user">
-                        <h2 className="branco">SUA AVALIAÇÃO</h2>
-                        <div className="avaliar-button">
-                            <img src={estrela_contornada} alt="Estrela Contornada Icon" />
-                            <p className="branco">Avaliar</p>
+                        <div className="avaliacao-user">
+                            <h2 className="branco">SUA AVALIAÇÃO</h2>
+                            <div className="avaliar-button">
+                                <img src={estrela_contornada} alt="Estrela Contornada Icon" />
+                                <p className="branco">Avaliar</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -36,7 +38,7 @@ function Main(props:{
                         <img src={props.imagemCapa} alt="Capa God Of War: Ragnarok" />
                     </div>
                    <div className="gameplay">
-                        <img src={props.imagemFundo} alt="Gameplay God Of War: Ragnarok" />
+                        <img src={props.imagemGameplay} alt="Gameplay God Of War: Ragnarok" />
                    </div>
                 </div>
             </div>
