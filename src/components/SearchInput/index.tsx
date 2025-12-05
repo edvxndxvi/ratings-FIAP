a6f88fe3de5df549c4eb577d3dd55af32b3c20c9
edvxndxvi <edvandavi1@hotmail.com>
2025-12-05 12:47:49 -0300

    Adicionando função de navageção ao clicar ENTER na busca

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,19 +1,29 @@
 import "./index.scss"
 import search_icon from "../../assets/svg/search-icon.svg"
 import { useSearch } from "../../context/searchContext";
-
+import { useNavigate } from "react-router-dom";
 
 export default function SearchInput() {
     const { searchQuery, setSearchQuery } = useSearch();
+    const navigate = useNavigate();
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         setSearchQuery(e.target.value)
     }
 
+    function enterSearch(e: React.KeyboardEvent<HTMLInputElement>) {
+        if(!searchQuery.trim()){
+            return
+        }
+        if(e.key === 'Enter'){
+            navigate('/')
+        }
+    }
+
     return (
         <div className="pesquisar">
             <img src={search_icon} alt="Search Icon" />
-            <input type="text" placeholder="Search" value={searchQuery} onChange={handleChange}/>
+            <input type="text" placeholder="Search" value={searchQuery} onChange={handleChange} onKeyDown={enterSearch}/>
         </div>
     )
 }
